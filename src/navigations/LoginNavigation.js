52200f8d4751db5b74af4bef1942c46a01524efa
arsham <arsham.ramezani@gmail.com>
2021-12-20 16:21:29 +0330

    feat: code made clean

--- src/navigations/LoginNavigation.js
+++ src/navigations/LoginNavigation.js
@@ -4,7 +4,7 @@ import React from 'react'
 import LoginPage from '../pages/auth/loginPage/LoginPage'
 import LandingPage from '../pages/auth/landingPage/LandingPage'
 import ForgetPassword from '../pages/auth/forgetPassword/ForgetPassword'
-import Welcome from "../pages/auth/welcom/Welcome";
+import Welcome from '../pages/auth/welcom/Welcome'
 import ConfirmCodePage from '../pages/auth/forgetPassword/confirmCodePage/ConfirmCodePage'
 const Stack = createStackNavigator()
 
@@ -14,7 +14,8 @@ export default function LoginNavigation() {
       <Stack.Screen name="landing-page" component={LandingPage} />
       <Stack.Screen name="login" component={LoginPage} />
       <Stack.Screen name="forget-password" component={ForgetPassword} />
-        <Stack.Screen name="welcome" component={Welcome} />
- <Stack.Screen name="confirm-code-page" component={ConfirmCodePage} />    </Stack.Navigator>
+      <Stack.Screen name="welcome" component={Welcome} />
+      <Stack.Screen name="confirm-code-page" component={ConfirmCodePage} />
+    </Stack.Navigator>
   )
 }
