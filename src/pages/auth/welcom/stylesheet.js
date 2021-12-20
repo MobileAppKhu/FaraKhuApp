856722ce5bfb09188cec0a02f6a358b514a60421
Mr.nobody <mohammad.rafiei.1380@gmail.com>
2021-12-20 15:59:35 +0330

    feat: add page welcome1

---
+++ src/pages/auth/welcom/stylesheet.js
@@ -0,0 +1,54 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+   container:{
+        flex: 1
+    },
+    image: {
+       width:80.14,
+        height:135,
+    },
+    imageContainer:{
+       justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 36,
+        marginBottom: 15,
+
+    },
+    texttype1:{
+       width: 170,
+       alignItems: 'center',
+        justifyContent:'center',
+
+    },
+    textview1:{
+       alignItems:'center',
+
+    },
+    typorgraphy1:{
+       textAlign:'center',
+
+    },
+    textview2:{
+       alignItems:'center',
+marginHorizontal:49,
+        marginTop: 38,
+        marginBottom: 43,
+    },
+    typorgraphy2:{
+       textAlign: 'center',
+
+    },
+    texttype2:{
+width:262,
+        alignItems: 'center',
+        justifyContent:'center',
+
+    },
+
+buttonview:{
+       marginHorizontal: 150,
+
+},
+})
+    export default styles
