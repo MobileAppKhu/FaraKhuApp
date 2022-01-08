2e8e6c81aad942d2eab15a6db816be7ff6badbd7
arsham <arsham.ramezani@gmail.com>
2022-01-08 15:52:22 +0330

    feat: simple header added

--- src/pages/main/Dashboard/Dashboard.js
+++ src/pages/main/Dashboard/Dashboard.js
@@ -1,15 +1,17 @@
 import {View} from 'react-native'
 import React from 'react'
-import DashboardHeader from './DashboardHeader/DashboardHeader'
+// import DashboardHeader from './DashboardHeader/DashboardHeader'
 import styles from './stylesheet'
 import {ScrollView} from 'react-native-gesture-handler'
 import UpcomingEvent from './UpcomingEvent/UpcomingEvent'
 import DashbaordMenu from './DashbaordMenu/DashbaordMenu'
+import SimpleHeader from '../../../components/SimpleHeader'
 // import NotificationsImages from './NotificationsImages/NotificationsImages'
 export default function Dashboard() {
   return (
     <View style={styles.root}>
-      <DashboardHeader />
+      {/* <DashboardHeader /> */}
+      <SimpleHeader title="Ridam to behnia" />
       <ScrollView>
         <UpcomingEvent />
         <View style={styles.seprator} />
