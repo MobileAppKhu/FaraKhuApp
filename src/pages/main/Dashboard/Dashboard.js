fab163ba551e2d2a59423ee22537c7355e449361
Hasan Mir <seyed.hasan.mir@gmail.com>
2022-02-01 19:58:28 +0330

    Add InternetManagement section

--- src/pages/main/Dashboard/Dashboard.js
+++ src/pages/main/Dashboard/Dashboard.js
@@ -6,12 +6,16 @@ import {ScrollView} from 'react-native-gesture-handler'
 import UpcomingEvent from './UpcomingEvent/UpcomingEvent'
 import DashbaordMenu from './DashbaordMenu/DashbaordMenu'
 import NotificationsImages from './NotificationsImages/NotificationsImages'
+import InternetManagement from './InternetManagement'
+
 export default function Dashboard() {
   return (
     <View style={styles.root}>
       <DashboardHeader />
       <ScrollView>
         <UpcomingEvent />
+        <View style={styles.seprator} />
+        <InternetManagement />
         <View style={styles.seprator} />
         <DashbaordMenu />
         <View style={styles.seprator} />
